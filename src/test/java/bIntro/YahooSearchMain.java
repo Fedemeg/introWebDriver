275e9348bb9565d352a735de0fdaf1f2b83a2dfa
Federico Meg <fede.meg@gmail.com>
2022-05-02 11:00:56 -0300

    actualizacion caso yahoo

--- src/test/java/bIntro/YahooSearchMain.java
+++ src/test/java/bIntro/YahooSearchMain.java
@@ -19,27 +19,27 @@ public class YahooSearchMain {
 		driver = new ChromeDriver();  //arranca el navegador, obvio Chrome
 		driver.manage().timeouts().implicitlyWait(5,  TimeUnit.SECONDS); //configura las esperas implicitas hasta 5 segundos
 		driver.get("http://www.yahoo.com"); //navega a la url que le indiquemos
-		WebElement searchBox = driver.findElement(By.id("header-search-input"));//busca un campo de busqueda usando el ID
-		WebElement searchButton = driver.findElement(By.id("header-desktop-search-button"));//busca el boton de busqueda por el ID
+		WebElement searchBox = driver.findElement(By.id("ybar-sbq"));//busca un campo de busqueda usando el ID
+		WebElement searchButton = driver.findElement(By.id("ybar-search"));//busca el boton de busqueda por el ID
 		
-		searchBox.clear();
-		searchBox.sendKeys("Selenium");
-		searchButton.click();
+		searchBox.clear();//limpia el campo
+		searchBox.sendKeys("Selenium");//escribe "Selenuim"
+		searchButton.click();//hacer click en el boton
 		
-		WebElement seleniumLink = driver.findElement(By.xpath("//h3[.='Selenium' and ./following-sibling::div[contains(.,'selenium.dev')] ] /a"));
-		seleniumLink.click();
+		WebElement seleniumLink = driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/div/div[1]/div/div/div/div/ol/li[1]/div/div[1]/h3/a"));//busca un link por xpath
+		seleniumLink.click();//le da click
 		
-		ArrayList<String> windowIds = new ArrayList<String>(driver.getWindowHandles());
-		System.out.println("Number of windows: " + windowIds.size());
+		ArrayList<String> windowIds = new ArrayList<String>(driver.getWindowHandles());//abre una nueva pestaña
+		System.out.println("Number of windows: " + windowIds.size()); //imprime la cantidad de pestañas que abrió
 		
 		for(String windowId: driver.getWindowHandles()) {
 			driver.switchTo().window(windowId);
 		}
 		
-		WebElement downloadLink = driver.findElement(By.linkText("Downloads"));
-		downloadLink.click();
+		WebElement downloadLink = driver.findElement(By.linkText("Downloads"));//busca otro link
+		downloadLink.click();//le da click
 		
-		driver.quit();
+		driver.quit();//cierra la ventana
 
 	}
 
